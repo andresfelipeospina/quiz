1fc8c011a914dd2ba7525abc880b761c0a0a70f5
andresfelipeospina <cefiahb@hotmail.com>
2015-07-13 17:44:25 +0200

    Ejercicio obligatorio del modulo 6

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,18 @@ var quizController = require('../controllers/quiz_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+	res.render('index', { title: 'Quiz' });
 });
 
+/* GET question. */
 router.get('/quizes/question', quizController.question);
+
+/* GET answer. */
 router.get('/quizes/answer', quizController.answer);
+
+/* GET author. */
+router.get('/author', function(req, res) {
+	res.render('author', { title: 'Quiz : author' });
+});
 
 module.exports = router;

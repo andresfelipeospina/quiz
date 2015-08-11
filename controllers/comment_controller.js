829fca0d4323333b991a3836a2e15d402a850350
andresfelipeospina <cefiahb@hotmail.com>
2015-08-11 19:34:17 +0200

    Ejercicio obligatorio modulo 9 tema 18

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -5,6 +5,22 @@
 var models = require('../models/models.js');
 var url = require('url');
 
+//Autoload - factoriza el codigo si la ruta incluye :commentId
+exports.load = function (req, res, next, commentId) {
+	console.log('commentId=' + commentId);
+	models.Comment.find({where:{id: Number(commentId)}}).then(
+			function (comment) {
+				if (comment) {
+					req.comment = comment;
+					next();
+				} else {
+					next(new Error('No existe commentId=' + commentId));
+				}
+			}
+	).catch(function (error) { next(error); });
+};
+
+
 // GET /quizes/:quizId(\\d+)/comments/new
 exports.new = function (req, res) {
 	res.render('comments/new.ejs', {quizId: req.params.quizId, errors: []});
@@ -24,4 +40,13 @@ exports.create = function (req, res) {
 			}); // redirección HTTP (URL relativo) lista de preguntas
 		}	
 	});
+};
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function (req, res) {
+	req.comment.publicado = true;
+	req.comment.save({fields: ['publicado']}).then(function () {
+		res.redirect('/quizes/' + req.params.quizId);
+	}
+			).catch(function (error) { next(error);});
 };
--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,9 @@ router.get('/', function(req, res) {
 	res.render('index', { title: 'Quiz', errors: [] });
 });
 
-// Autoload de comandos con :quizId
+// Autoload de comandos
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definición de rutas de session
 router.get('/login', sessionController.new); // formulario login
@@ -28,10 +29,10 @@ router.get('/quizes/:quizId(\\d+)/edit', sessionController.loginRequired, quizCo
 router.put('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizController.destroy);
 
-//Definicion de rutas de /quizes... comments
-router.get('/quizes/:quizId(\\d+)/comments/new', quizController.new);
-router.post('/quizes/:quizId(\\d+)/comments/', quizController.create);
-
+//Definicion de rutas de /comentarios ... comments
+router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments/', commentController.create);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 /* GET author. */
 router.get('/author', function(req, res) {
